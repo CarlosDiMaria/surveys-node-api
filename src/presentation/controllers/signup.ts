79de9efc4225c7950b53b59990a071e59ad74d3b
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-04 23:21:18 -0300

    refactor: imports for errors

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,8 @@
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
-import { MissingParamError } from '../erros/missing-param-error'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from '../erros/invalid-param-error'
+import { InvalidParamError, MissingParamError } from '../erros/'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
