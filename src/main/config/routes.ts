2005295e6fd5a36a8e4a7a9039c9fb5d8b8bf4d6
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-10 15:33:56 -0300

    refactor: prepare ts build and deploy scripts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,8 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import LoginRoutes from '../routes/login-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async (file) => {
-    const route = (await import(`../../../${file}`)).default
-    route(router)
-  })
+  LoginRoutes(router)
 }
