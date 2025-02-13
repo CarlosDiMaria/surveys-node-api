fa812fe660940032e27ef19ff1dd93a51dc5b5a9
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-13 14:31:49 -0300

    feature: implement DbLoadAccountByToken

--- src/data/protocols/load-account-repository.ts
+++ src/data/protocols/load-account-repository.ts
@@ -3,3 +3,7 @@ import { AccountModel } from '../../domain/models/account'
 export interface LoadAccountByEmailRepository {
   loadByEmail: (email: string) => Promise<AccountModel | null>
 }
+
+export interface LoadAccountByTokenRepository {
+  loadByToken: (token: string, role?: string) => Promise<AccountModel | null>
+}
