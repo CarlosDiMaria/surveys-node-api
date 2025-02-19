e5d6d6034c22a4eb43cb3a58fd304e8b49931d82
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-19 15:26:55 -0300

    feature: create docs for login API on swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,52 @@
+import { accountSchema } from './schemas/account-schema'
+import { loginSchema } from './schemas/login-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Enquetes Node API',
-    description: 'Esta é uma API para criação de enquetes entre programadores.',
+    description: 'This is an API for creating polls among programmers.',
     version: '1.0.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [{
+    name: 'login'
+  }],
+  paths: {
+    '/login': {
+      post: {
+        tags: ['login'],
+        summary: 'API for user authentication.',
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/schemas/loginSchema'
+              }
+            }
+          }
+        },
+        responses: {
+          200: {
+            description: 'Sucesso',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/schemas/accountSchema'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  schemas: {
+    accountSchema,
+    loginSchema
   }
 }
