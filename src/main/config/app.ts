05ae8be8575622247169ea957cc35b7e52768b40
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-19 14:51:59 -0300

    feature: initial swagger config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setUpMiddlewares from './middlewares'
 import setUpRoutes from './routes'
+import SetupSwagger from './config-swagger'
 
 const app = express()
+SetupSwagger(app)
 setUpMiddlewares(app)
 setUpRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Enquetes Node API',
+    description: 'Esta é uma API para criação de enquetes entre programadores.',
+    version: '1.0.0'
+  }
+}
