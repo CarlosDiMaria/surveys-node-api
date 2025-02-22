0c004fd03328a412bee3eae82650881aed88520a
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-22 16:38:14 -0300

    feature: create documentation for lod survey result get route

--- src/main/docs/paths/surveys-result-path.ts
+++ src/main/docs/paths/surveys-result-path.ts
@@ -38,5 +38,36 @@ export default {
         $ref: '#/components/forbiddenErrorComponent'
       }
     }
+  },
+  get: {
+    security: [{ apiKeyAuthSchema: [] }],
+    tags: ['Surveys'],
+    summary: 'API to load the answers of a survey poll.',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResultSchema'
+            }
+          }
+        }
+      },
+      500: {
+        $ref: '#/components/serverErrorComponent'
+      },
+      403: {
+        $ref: '#/components/forbiddenErrorComponent'
+      }
+    }
   }
 }
--- src/main/docs/schemas/surveyResultSchema.ts
+++ src/main/docs/schemas/surveyResultSchema.ts
@@ -10,7 +10,7 @@ export const surveyResultSchema = {
     answers: {
       type: 'array',
       items: {
-        $ref: '#/schemas/surveyResultAnswer'
+        $ref: '#/schemas/surveyResultAnswerSchema'
       }
     },
     date: {
