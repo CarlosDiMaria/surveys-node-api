67b51cea55bd97ccd21c64f0fe85f1e6ff24824d
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-25 23:30:50 -0300

    feature: add signup route graphql

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,12 @@
 import { adaptResolver } from '../../adapters/apollo-server-resolver-adapter'
 import { makeLoginController } from '../../factories/login'
+import { makeSignUpController } from '../../factories/signup'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args)
+  },
+  Mutation: {
+    signUp: async (parent: any, args: any) => adaptResolver(makeSignUpController(), args)
   }
 }
