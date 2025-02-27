9952d4baee19331ba8e60340b94f2d5e0cd7a1c5
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-02-26 22:34:57 -0300

    feature: load surveys configured in graphql

---
+++ src/main/graphql/resolvers/survey.ts
@@ -0,0 +1,8 @@
+import { adaptResolver } from '../../adapters/apollo-server-resolver-adapter'
+import { makeLoadSurveyController } from '../../factories/load-surveys'
+
+export default {
+  Query: {
+    load: async (parent: any, args: any) => adaptResolver(makeLoadSurveyController(), args)
+  }
+}
---
+++ src/main/graphql/typedefs/survey.ts
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-server-express'
+
+export default gql`
+  extend type Query {
+    load: [Survey]
+  }
+
+  type SurveyAnswerModel {
+    image: String!
+    answer: String!
+  }
+
+  type Survey {
+    id: ID!
+    question: String!
+    answers: [SurveyAnswerModel]
+    date: String!
+  }
+`
